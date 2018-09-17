964baa962a6f22bbe6ef291e119d70a646af4f4e
Ben523 <kanomaroi555@gmail.com>
2018-09-17 15:02:17 +0700

    add firestore

--- app/src/main/java/com/example/a58070067/healthy/WeightFormFragment.java
+++ app/src/main/java/com/example/a58070067/healthy/WeightFormFragment.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 public class WeightFormFragment extends Fragment{
+
+    private FirebaseFirestore  mdb;
+    private FirebaseAuth mAuth;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,8 +31,17 @@ public class WeightFormFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mdb = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        mdb.setFirestoreSettings(settings);
+        saveWeight();
         backBtn();
     }
+
+
 
     private void backBtn()
     {
@@ -42,7 +63,28 @@ public class WeightFormFragment extends Fragment{
         addWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                EditText date = getView().findViewById(R.id.weight_date);
+                EditText weight = getView().findViewById(R.id.weight_value);
+
+                String dateTxt = date.toString();
+                String weightTxt = weight.toString();
+                String user_id = mAuth.getCurrentUser().getUid();
+                Weight weight1 = new Weight(dateTxt,60,"UP");
+                Log.d("USER",weightTxt);
+                mdb.collection("Healthy").document(user_id)
+                        .collection("weight").document(dateTxt).set(weight1)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getActivity(),"SAVE WEIGHT",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "ERROR :"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
             }
         });
     }
